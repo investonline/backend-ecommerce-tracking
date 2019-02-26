d991b0b645776789370c8a50ebed0b04c40ef1ae
Peter Verboom <pverboom@gmail.com>
2019-02-26 15:33:44 +0100

    Added custom dimensions

--- src/GoogleAnalytics/Entities/EnhancedTransaction.php
+++ src/GoogleAnalytics/Entities/EnhancedTransaction.php
@@ -8,7 +8,6 @@ namespace InvestOnline\GoogleAnalytics\Entities;
  */
 class EnhancedTransaction extends Entity
 {
-
     /**
      * @var array
      */
@@ -18,15 +17,25 @@ class EnhancedTransaction extends Entity
      * @var array
      */
     protected $fields = [
-        'ti'    => 'id',
-        'ta'    => 'affiliation',
-        'tr'    => 'revenue',
-        'ts'    => 'shipping',
-        'tt'    => 'tax',
-        'cu'    => 'currency',
-        'pal'   => 'list',
+        'ti'   => 'id',
+        'ta'   => 'affiliation',
+        'tr'   => 'revenue',
+        'ts'   => 'shipping',
+        'tt'   => 'tax',
+        'cu'   => 'currency',
+        'tcc'  => 'coupon',
+        'pal'  => 'list',
+        'cd1'  => 'dimension1',
+        'cd2'  => 'dimension2',
+        'cd3'  => 'dimension3',
+        'cd4'  => 'dimension4',
+        'cd5'  => 'dimension5',
+        'cd6'  => 'dimension6',
+        'cd7'  => 'dimension7',
+        'cd8'  => 'dimension8',
+        'cd9'  => 'dimension9',
+        'cd10' => 'dimension10',
     ];
-
 
     /**
      * Custom toQuery method
